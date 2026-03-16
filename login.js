80a88ef2e49f43a5d9fc1712182e3aa7edf9d0c7
Nerd29 <majumderturja59@gmail.com>
2026-03-15 20:42:46 -0700

    login,navbar done

---
+++ login.js
@@ -0,0 +1,25 @@
+const signinBTN=document.getElementById('signin-btn').addEventListener('click',function(){
+    // console.log('login button clicked')
+    const username=document.getElementById('username-btn')
+    const input=username.value ;
+    // console.log(inputBtn)
+
+
+const passwordBtn=document.getElementById('password-btn')
+const password=passwordBtn.value ;
+// console.log(digitalBtn)
+
+if(input=="admin" && password=='admin123'){
+    alert('Login Successful!!!!')
+
+    window.location.assign("/indexhome.html")
+}
+else{
+    alert('Login Failed!!!')
+    username.value="";
+    passwordBtn.value="";
+    return;
+}
+
+
+})
